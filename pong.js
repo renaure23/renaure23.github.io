c65e696c05066962100b8848f54b51f1c6ecaa7a
renaure23 <30881546+renaure23@users.noreply.github.com>
2017-09-07 09:26:06 +1000

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,1 +1,19 @@
-
+var animate = window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  function(callback) { window.setTimeout(callback, 1000/60) };
+var canvas = document.createElement('canvas');
+var width = 600;
+var height = 800;
+canvas.width = width;
+canvas.height = height;
+var context = canvas.getContext('2d');
+window.onload = function() {
+  document.body.appendChild(canvas);
+  animate(step);
+};
+var step = function() {
+  update();
+  render();
+  animate(step);
+};
